aa5b448c438396d2509eeb23d14b04b57698e277
mrivera06 <mariely.rivera@patelhs.org>
2020-11-05 14:26:09 +0000

    I added an array of strings

--- Main.java
+++ Main.java
@@ -1,8 +1,29 @@
 class Main {
   public static void main(String[] args) {
-    int[] seats = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    int[ ] seats = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
-    String [] names = new String[15]; 
+    String [ ] names = new String[15]; 
     // Add an array of Strings called names 
+    names [0] = "";
+    names [1] = "";
+    names [2] = "";
+    names [3] = "";
+    names [4] = "";
+    names [5] = "";
+    names [6] = "";
+    names [7] = "";
+    names [8] = "";
+    names [9] = "";
+    names [10] = "";
+    names [11] = "";
+    names [12] = "";
+    names [13] = "";
+    names [14] = "";
+
+    for (String i = 0; i < names.length; i++);
+    {
+      System.out.println( names[i] );
+    }  
+
   }
 }
