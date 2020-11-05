edd7094bf37a2c1ce4713e20d106172ade2bb91b
mrivera06 <mariely.rivera@patelhs.org>
2020-11-05 14:13:00 +0000

    I added an array of int

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int[] seats = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+    String [] names = new String[15]; 
+    // Add an array of Strings called names 
   }
 }
