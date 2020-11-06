bcab209747069d80244d65b3f0a38ff4e3e48014
mrivera06 <mariely.rivera@patelhs.org>
2020-11-06 18:37:26 +0000

    I finsihed :)

--- Main.java
+++ Main.java
@@ -1,34 +1,23 @@
+import java.util.Scanner;
 class Main {
-  public static void main(String[] args) {
+        public static void main(String[] args){
     int[ ] seats = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
-    String [ ] names = new String[15]; 
+    String [ ] name = new String[15]; 
     // Add an array of Strings called names 
-    names [0] = "";
-    names [1] = "";
-    names [2] = "";
-    names [3] = "";
-    names [4] = "";
-    names [5] = "";
-    names [6] = "";
-    names [7] = "";
-    names [8] = "";
-    names [9] = "";
-    names [10] = "";
-    names [11] = "";
-    names [12] = "";
-    names [13] = "";
-    names [14] = "";
-
-    for (int i = 15; i < names.length; i++);
+    Scanner Scanner = new Scanner(System.in);
+    for (int i = 0; i < 15; i++)
     {
 
       Scanner scan = new Scanner(System.in);
-
       System.out.print("What is your name?");
-      String names = in.nextLine();
+     name[i]= Scanner.next();
+      System.out.println(name[i]+ "  You will sit in seat "  + seats [i]);
 
-    }  
-
+    } 
+    int i= 0;
+    while (i <15){
+      System.out.println(name[i]+"you will sit in " + seats [i]);
+    i++;}
   }
 }
